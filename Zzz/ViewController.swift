d383d2f3302ade58add88af22ba88f03fb5e17e6
Roma Laitarenko <rlaitarenko@iconnectpos.com>
2016-06-29 23:22:00 +0300

    Initial Commit

---
+++ Zzz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Zzz
+//
+//  Created by Roman Laitarenko on 6/29/16.
+//  Copyright © 2016 Roman Laitarenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
